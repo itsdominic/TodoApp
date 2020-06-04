4b1fff8f48a8bcbf6b83fba9aa22b243b0f72fac
Dominic Charles <dominic@macbook-pro.home>
2020-06-04 23:53:38 +0100

    add todo filter

--- script.js
+++ script.js
@@ -25,24 +25,49 @@ const todoList = [
   },
 ];
 
-const todosLeft = todoList.filter(function (todo) {
-  return !todo.completed;
-});
+const filters = {
+  searchText: "",
+};
 
-const todoSummary = document.createElement("h2");
-todoSummary.textContent = `You have ${todosLeft.length} todos left to complete`;
-document.querySelector("body").appendChild(todoSummary);
+const renderTodos = function (todoList, filters) {
+  const filteredTodos = todoList.filter(function (todo) {
+    return todo.text.toLowerCase().includes(filters.searchText.toLowerCase());
+  });
 
-todoList.forEach(function (todo) {
-  const todoitem = document.createElement("p");
-  todoitem.textContent = todo.text;
-  document.querySelector("body").appendChild(todoitem);
-});
+  const todosLeft = filteredTodos.filter(function (todo) {
+    return !todo.completed;
+  });
+
+  document.querySelector("#todo-content").innerHTML = "";
+
+  const todoSummary = document.createElement("h2");
+  todoSummary.textContent = `You have ${todosLeft.length} todos left to complete`;
+  document.querySelector("#todo-content").appendChild(todoSummary);
+
+  filteredTodos.forEach(function (todo) {
+    const newTodo = document.createElement("p");
+    newTodo.textContent = todo.text;
+    document.querySelector("#todo-content").appendChild(newTodo);
+  });
+};
+
+renderTodos(todoList, filters);
+
+// todoList.forEach(function (todo) {
+//   const todoitem = document.createElement("p");
+//   todoitem.textContent = todo.text;
+//   document.querySelector("body").appendChild(todoitem);
+// });
 
 document.querySelector("#add-todo").addEventListener("click", function () {
   console.log("A new todo has been added");
 });
 
-document.querySelector("todo-text").addEventListener("input", function (e) {
+document.querySelector("#todo-text").addEventListener("input", function (e) {
   console.log(e.target.value);
+});
+
+document.querySelector("#search-text").addEventListener("input", function (e) {
+  filters.searchText = e.target.value;
+  renderTodos(todoList, filters);
 });
