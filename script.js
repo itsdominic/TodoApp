8d02812d9b524ae29c4bb6aab1dbbd24073ea14f
Dominic Charles <dominic@macbook-pro.home>
2020-06-07 20:18:18 +0100

    refactor code

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let todoList = [];
+const todoList = getSavedTodos();
 
 const filters = {
   searchText: "",
@@ -6,100 +6,30 @@ const filters = {
 };
 
 
-// create a variable which reads the items from local storage and stores them  
-const todoListJSON = localStorage.getItem("todoList")
-
-// if there are items in local storage it converts back to a JS object using parse 
-if (todoListJSON !== null) {
-  todoList = JSON.parse(todoListJSON)
-}
-
-const renderTodos = function (todoList, filters) {
-  const filteredTodos = todoList.filter(function (todo) {
-
-    // searchTextMatch is only true when the text input matches a todo which includes same text 
-    const searchTextMatch = todo.text.toLowerCase().includes(filters.searchText.toLowerCase());
-
-    // Is the checkbox unchecked (false) or is the todo false
-    const hideCompletedMatch = !filters.hideCompleted || !todo.completed;
-
-
-    // Only when the searchedTextMatch above is true and one of the hideCompleted statments are true
-    // this return equal true and therefore creates a new array of todos in the 
-    // filtredTodos const variable. 
-    // If this statement equals false the orginal todoList will be rendered
-
-    return searchTextMatch && hideCompletedMatch;
-  });
-
-
-  // This filters all the todos which have a compelted of false.
-  // It takes the filteredTodos variable from above and createds a new array in the todoLeft variable
-  const todosLeft = filteredTodos.filter(function (todo) {
-    return !todo.completed;
-  });
-
-
-  // Before any todos are rendered this clears the HTML 
-  document.querySelector("#todo-content").innerHTML = "";
-
-  // This creates a h2 element 
-  const todoSummary = document.createElement("h2");
-
-  // This makes the text content of the created h2 element the length of 
-  // the array which was created in todosLeft variable 
-  todoSummary.textContent = `You have ${todosLeft.length} todos left to complete`;
-
-  // This appeneds the todoSumary to the selected HTML element
-  document.querySelector("#todo-content").appendChild(todoSummary);
-
-  // Runs a forEach over the filteredTodos arrays
-  filteredTodos.forEach(function (todo) {
-
-    // For each todo it creates a p element in the newTodo variable 
-    const newTodo = document.createElement("p");
-
-    // It assigns this elements text to the todos text 
-    newTodo.textContent = todo.text;
-
-    // This appends it to the HTML element 
-    document.querySelector("#todo-content").appendChild(newTodo);
-  });
-};
-
-// Runs RenderTodos with the two arguements 
 renderTodos(todoList, filters);
 
 
 // Event Listener for the input field 
 document.querySelector("#search-text").addEventListener("input", function (e) {
-
   // Changes the filters object to the value of the input field being targeted 
   filters.searchText = e.target.value;
-
   // RendersTodos 
   renderTodos(todoList, filters);
 });
 
 // Event Listener for the form input button field 
 document.querySelector("#add-todo").addEventListener("submit", function (e) {
-
   // Prevents deafult action of the submit button
   e.preventDefault();
 
   // Pushes a new todo to the todoList using
   todoList.push({
-
     // The text of the new todo is the input fields value that user types in
     text: e.target.elements.addTodo.value,
     completed: false,
   });
 
-  // setting localstorage with key todoList 
-  // our value the todoList which is converted to a string 
-  localStorage.setItem("todoList", JSON.stringify(todoList))
-
-  // RendersTodos 
+  saveTodos(todoList)
   renderTodos(todoList, filters);
 
   // Clears the input field 
@@ -108,10 +38,8 @@ document.querySelector("#add-todo").addEventListener("submit", function (e) {
 
 // Event Listeners for checkbox 
 document.querySelector("#completed").addEventListener("change", function (e) {
-
   // Changes filters object of hideCompleted to depend on if the box is checked or not 
   filters.hideCompleted = e.target.checked;
-
   // RendersTodos list
   renderTodos(todoList, filters);
 });
---
+++ todo-functions.js
@@ -0,0 +1,78 @@
+// Get any saved todos from Local Storage 
+const getSavedTodos = function () {
+
+    // Creat variiable which stores the "todoList" Key 
+    const todoListJSON = localStorage.getItem("todoList")
+
+    // If the todoList object is not null we parse the DATA and return it 
+    // If it is null we return a empty array
+    if (todoListJSON !== null) {
+        return JSON.parse(todoListJSON)
+    } else {
+        return []
+    }
+
+}
+
+
+// This saves the Todos we create. Takes in todoList argument 
+const saveTodos = function (todoList) {
+    // Sets local items to the key todoList and makes the todolist a string 
+    localStorage.setItem("todoList", JSON.stringify(todoList))
+
+}
+
+
+
+const renderTodos = function (todoList, filters) {
+    const filteredTodos = todoList.filter(function (todo) {
+        // searchTextMatch is only true when the text input matches a todo which includes same text 
+        const searchTextMatch = todo.text.toLowerCase().includes(filters.searchText.toLowerCase());
+        // Is the checkbox unchecked (false) or is the todo false
+        const hideCompletedMatch = !filters.hideCompleted || !todo.completed;
+
+        // Only when the searchedTextMatch above is true and one of the hideCompleted statments are true
+        // this return equal true and therefore creates a new array of todos in the 
+        // filtredTodos const variable. 
+        // If this statement equals false the orginal todoList will be rendered
+        return searchTextMatch && hideCompletedMatch;
+    });
+
+    // This filters all the todos which have a compelted of false.
+    // It takes the filteredTodos variable from above and createds a new array in the todoLeft variable
+    const todosLeft = filteredTodos.filter(function (todo) {
+        return !todo.completed;
+    });
+
+    // Before any todos are rendered this clears the HTML 
+    document.querySelector("#todo-content").innerHTML = "";
+
+    // Runs the generateSummary function with the todoLeft filter as argument.
+    document.querySelector("#todo-content").appendChild(generateSummary(todosLeft));
+
+    // Runs a forEach over the filteredTodos arrays
+    filteredTodos.forEach(function (todo) {
+        // This appends it to the HTML element 
+        document.querySelector("#todo-content").appendChild(generateTodo(todo));
+    });
+};
+
+
+// Generate the todo element. Takes in the individual todo element as this will be in a filter array method
+const generateTodo = function (todo) {
+    const newTodo = document.createElement("p");
+    // It assigns this elements text to the todos text 
+    newTodo.textContent = todo.text;
+
+    return newTodo
+}
+
+// Generate the summary using the Todoleft filter as a argument so we can have access to the length
+const generateSummary = function (todosLeft) {
+    const todoSummary = document.createElement("h2");
+    // This makes the text content of the created h2 element the length of 
+    // the array which was created in todosLeft variable 
+    todoSummary.textContent = `You have ${todosLeft.length} todos left to complete`;
+    return todoSummary
+
+}
